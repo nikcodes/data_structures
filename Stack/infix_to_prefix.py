05a37c0479c9251557cc0196388dbe8a5aa08380
Nikhil Rathore <35673596+nikcodes@users.noreply.github.com>
2018-07-24 18:59:56 +0530

    Create infix_to_prefix.py

---
+++ Stack/infix_to_prefix.py
@@ -0,0 +1,31 @@
+s=input()
+new=''
+for c in s[::-1]:
+    if c=='(':
+        new+=')'
+    elif c==')':
+        new+='('
+    else:
+        new+=c
+s=new
+a=[]
+op=['+','-','*','/','//']
+d={}
+d['+']=d['-']=1
+d['*']=d['/']=d['//']=2
+ans=[]
+for c in s:
+    if c.isalnum():
+        ans.append(c)
+    elif c==')':
+        while a and a[-1]!='(':
+            ans.append(a.pop())
+        a.pop()
+    elif c=='(':
+        a.append('(')
+    else:
+        while a and a[-1]!='(' and d[a[-1]]>=d[c]:
+            ans.append(a.pop())
+        a.append(c)
+ans.extend(a[::-1])
+print(''.join(ans[::-1]))
