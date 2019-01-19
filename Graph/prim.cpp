1d0d9e876751661632587dc8e6f0631dcf22e16b
Nikhil Rathore <35673596+nikcodes@users.noreply.github.com>
2019-01-19 21:06:12 +0530

    Create prim.cpp

---
+++ Graph/prim.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include<cstdio>
+#define ll long long int
+#define mod 1000000007
+#define pi 3.141592653589793
+#define f first
+#define s second
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define vfor(e, a) for (vector<ll> :: iterator e = a.begin(); e != a.end(); e++)
+#define vfind(a, e) find(a.begin(), a.end(), e)
+#define forr(i, n) for (ll i = 0; i < n; i++)
+#define rfor(i, n) for (ll i = n - 1; i >= 0; i--)
+#define fors(i, b, e, steps) for(ll i = b; i < e; i += steps)
+#define rfors(i, e, b, steps) for(ll i = e; i > b; i -= steps)
+#define mp make_pair
+using namespace std;
+
+void down(pair<ll, ll> a[], ll n, ll i, ll indArray[]) {
+    ll left = 2 * i + 1;
+    ll right = 2 * i + 2;
+    ll ind = i;
+    ll m = a[i].s;
+
+    if (left < n && a[left].s < m) {
+        m = a[left].s;
+        ind = left;
+    }
+
+    if (right < n && a[right].s < m) {
+        ind = right;
+    }
+
+    if (ind != i) {
+        auto t = a[i];
+        a[i] = a[ind];
+        a[ind] = t;
+
+        indArray[a[ind].f] = ind;
+        indArray[a[i].f] = i;
+
+        down(a, n, ind, indArray);
+
+    }
+}
+
+
+void up(pair<ll, ll> a[], ll n, ll i, ll indArray[]) {
+    ll ind = (i - 1) / 2;
+    while (ind >= 0 && a[ind].s > a[i].s) {
+        auto t = a[i];
+        a[i] = a[ind];
+        a[ind] = t;
+
+        indArray[a[ind].f] = ind;
+        indArray[a[i].f] = i;
+
+        i = ind;
+        ind = (i - 1) / 2;
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+//    cout << setprecision(10);
+
+
+    ll n, m;
+    cin >> n >> m;
+    vector<pair<ll, ll>> a[n];
+    bool visited[n];
+    fill(visited, visited + n, false);
+
+    forr(i, m) {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        a[u].emplace_back(mp(v, w));
+        a[v].emplace_back(mp(u, w));
+    }
+
+    ll parent[n];
+    fill(parent, parent + n, -1);
+
+    pair<ll, ll> key[n];
+    forr(i, n) {
+        key[i] = mp(i, INT_MAX);
+    }
+
+    key[0].s = 0;
+    ll ind[n];
+    iota(ind, ind + n, 0);
+
+    ll minw = 0;
+    ll size = n;
+
+    forr(i, n) {
+        ll u = key[0].f;
+        visited[u] = true;
+        minw += key[0].s;
+        key[0] = key[size - 1];
+        ind[key[0].f] = 0;
+        size--;
+        down(key, size, 0, ind);
+
+        for (auto e : a[u]) {
+            if (visited[e.f]) {
+                continue;
+            }
+
+            ll v = e.f;
+            ll w = e.s;
+            ll j = ind[v];
+
+            if (key[j].s > w) {
+                key[j].s = w;
+                parent[v] = u;
+                up(key, n, j, ind);
+            }
+        }
+    }
+
+    cout << minw << '\n';
+    forr (i, n) {
+        cout << parent[i] << ' ' << i << '\n';
+    }
+    
+}
+
+//9 14
+//7 6 1
+//8 2 2
+//6 5 2
+//0 1 4
+//2 5 4
+//8 6 6
+//2 3 7
+//7 8 7
+//0 7 8
+//1 2 8
+//3 4 9
+//5 4 10
+//1 7 11
+//3 5 14
